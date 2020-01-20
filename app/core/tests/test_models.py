1751ba0fbd94a7bba1b94d49ebdffb7b7c63a018
adamgreenberg <adam.m.t.greenberg@gmail.com>
2020-01-20 15:12:05 -0800

    Formatting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase):
 
+class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test Creating a new user with an email is successful"""
@@ -16,7 +16,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normailized(self):
         """"Test the email for a new user is normailzed"""
         email = 'test@ADAM.COM'
@@ -24,12 +23,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-
     def test_new_user_invalid_email(self):
         """Test creating email with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
